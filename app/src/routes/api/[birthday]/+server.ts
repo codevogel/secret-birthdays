f72c879d7776b3fe44058b58b27dd82c0b9bd6cc
Kamiel de Visser <kamieldevisser@gmail.com>
2025-07-13 16:08:23 +0200

    mv api route

--- app/src/routes/api/[birthday]/+server.ts
+++ app/src/routes/api/[birthday]/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
-import { BinaryBirthdayGenerator, PiBirthdayGenerator } from '$lib/types/BirthdayGenerator';
+import { BinaryBirthdayGenerator } from '$lib/types/BirthdayGenerator';
 
 export const GET: RequestHandler = async ({ params }) => {
 	const YYYY_MM_DD_REGEX: RegExp = /^\d{4}-\d{2}-(0[1-9]|1[0-2])$/;
@@ -12,8 +12,5 @@ export const GET: RequestHandler = async ({ params }) => {
 
 	const birthday = new Date(birthdayParam);
 	const binaryBirthdayGenerator = new BinaryBirthdayGenerator(birthday, 32 + 1);
-	const piBirthdayGenerator = new PiBirthdayGenerator(birthday, 5 + 1);
-
-	console.log(piBirthdayGenerator.generate());
 	return json(binaryBirthdayGenerator.generate());
 };
