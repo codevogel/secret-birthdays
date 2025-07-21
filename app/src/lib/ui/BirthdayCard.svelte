ec1ad5812c6a422ab72cb4724c403b022a6abad9
Kamiel de Visser <kamieldevisser@gmail.com>
2025-07-21 17:52:49 +0200

    add birthdaycard

---
+++ app/src/lib/ui/BirthdayCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { CalendarEvent } from '$lib/types/Calendar';
+	import Card from '$lib/ui/Card.svelte';
+
+	let { event }: { event: CalendarEvent } = $props();
+</script>
+
+<Card>
+	{#snippet header()}
+      {event.title}
+	{/snippet}
+	{#snippet article()}
+		<p class="text-surface-300 text-sm">{event.description}</p>
+	{/snippet}
+</Card>
