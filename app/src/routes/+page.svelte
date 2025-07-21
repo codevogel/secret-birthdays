1c15a6d4fca4acf62baf1ab3affed51161ee716c
Kamiel de Visser <kamieldevisser@gmail.com>
2025-07-21 17:52:59 +0200

    show birthday cards

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	import { isOnSameDay } from '$lib/util/date';
 	import TimeLine from '$lib/ui/TimeLine.svelte';
 	import CalendarTimeLineSwitch from '$lib/ui/CalendarTimeLineSwitch.svelte';
+	import BirthdayCard from '$lib/ui/BirthdayCard.svelte';
 
 	let { data }: { data: { birthdayGenerationResults: BirthdayGenerationResults | null } } =
 		$props();
@@ -31,31 +32,29 @@
 		return events;
 	});
 
-	let eventOnSelectedDate: CalendarEvent | null = $derived.by(
-		() => events.find((event) => isOnSameDay(event.date, selectedDate)) || null
+	let eventsOnSelectedDate: CalendarEvent[] = $derived.by(
+		() => events.filter((event) => isOnSameDay(event.date, selectedDate)) || null
 	);
 
 	let mode: 'calendar' | 'timeline' = $state('calendar');
 </script>
 
 <div class="grid min-h-full grid-cols-1 items-center p-4">
-	<div class="flex flex-row justify-between">
-		<BirthdayPickerModal />
-		<CalendarTimeLineSwitch bind:mode />
-	</div>
 	{#if birthdayGenerationResults}
+		<div class="flex flex-row justify-between">
+			<BirthdayPickerModal />
+			<CalendarTimeLineSwitch bind:mode />
+		</div>
 		{#if mode === 'calendar'}
 			<Calendar bind:selectedDate {events} />
 
 			<div class="m-4 flex flex-col items-center justify-center text-left">
-				<div>
-					{#if eventOnSelectedDate}
-						<p class="text-primary-500">
-							You have a {eventOnSelectedDate.title} on {eventOnSelectedDate.date.toLocaleDateString()}!
-						</p>
-						<p class="text-primary-500">
-							Description: {eventOnSelectedDate.description}
-						</p>
+				<div class="flex flex-col gap-4">
+					{#if eventsOnSelectedDate.length > 0}
+						<strong>On this date:</strong>
+						{#each eventsOnSelectedDate as event, i (i)}
+							<BirthdayCard {event} />
+						{/each}
 					{:else}
 						<p class="text-primary-500">You have no secret birthdays on this date.</p>
 						<p class="text-surface-300 text-[.75rem]">
@@ -69,5 +68,24 @@
 		{:else}
 			<TimeLine {events} />
 		{/if}
+	{:else}
+		<div class="grid max-w-md grid-cols-1 place-items-center gap-4 text-center">
+			<strong class="text-xl">Discover your secret birthdays!</strong>
+			<p>You’ve had more birthdays than you think.</p>
+			<p>
+				Sure, we celebrate our birthday every <span class="code bg-primary-500"
+					>365.25 days</span
+				>...<br />But what about your
+				<span class="code">1,000th day</span>?<br />Or turning
+				<span class="code">3 π-years</span>
+				old on day
+				<span class="code">314</span>?
+			</p>
+			<p>
+				Enter your birth date to reveal fun life milestones based on primes, palindromes, binary
+				numbers, and more.
+			</p>
+			<BirthdayPickerModal />
+		</div>
 	{/if}
 </div>
