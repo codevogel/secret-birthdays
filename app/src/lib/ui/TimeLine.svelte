ff4fc8a9ed1f8b6d73752ea5811880c4cb177125
Kamiel de Visser <kamieldevisser@gmail.com>
2025-07-23 16:30:38 +0200

    timeline formatting

--- app/src/lib/ui/TimeLine.svelte
+++ app/src/lib/ui/TimeLine.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { CalendarEvent } from '$lib/types/Calendar';
-	import TimeLineCard from '$lib/ui/TimeLineCard.svelte';
+	import { Squircle } from 'lucide-svelte';
 
 	let { events = [] }: { events: CalendarEvent[] } = $props();
 
@@ -52,22 +52,58 @@
 		year: TimeLineYear;
 		events: CalendarEvent[];
 	}
+
+	function monthString(month: number): string {
+		const months = [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dev'
+		];
+		return months[month];
+	}
 </script>
 
-<div class="">
+<div class="grid grid-cols-1 py-4">
 	{#each years as year, i (i)}
-		<h1 class="h1 text-primary-500 mt-8 underline">{year.year}</h1>
-		{#each year.months as month, j (j)}
-			<h2 class="h3 text-primary-300 my-4">
-				{new Date(year.year, month.month).toLocaleString('default', { month: 'long' })}
-			</h2>
-			<div class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-4">
-				{#each month.days as day, k (k)}
-					{#each day.events as event, l (l)}
-						<TimeLineCard {event} />
+		<div class="text-primary-500 font-bold text-xl">{year.year}</div>
+		<div class="bg-primary-500 ms-6 h-4 w-0.5"></div>
+		<div class="grid grid-cols-1">
+			{#each year.months as month, j (j)}
+				<div class="ps-2.5">{monthString(month.month)}</div>
+				<div class="grid grid-cols-[auto_1fr]">
+					{#each month.days as day, k (k)}
+						<div class="bg-primary-500 relative ms-6 h-full w-0.5">
+							<Squircle
+								class="fill-primary-500 text-primary-500 absolute top-[20px] right-[-5px]"
+								size={12}
+							/>
+						</div>
+						<div class="grid grid-cols-[auto_1fr]">
+							<div class="text-primary-500 w-[5ch] ps-4 pt-3.5">{day.day.toString().padStart(2, '0')}</div>
+							<div class="mb-4 grid grid-cols-1 gap-y-4">
+								{#each day.events as event, l (l)}
+									<div class="card max-w-sm bg-surface-600 flex flex-col p-4">
+										<header class="font-bold text-primary-50">{event.title}</header>
+                              <article>{event.description}</article>
+									</div>
+								{/each}
+							</div>
+						</div>
 					{/each}
-				{/each}
-			</div>
-		{/each}
+				</div>
+			{/each}
+		</div>
+		{#if i !== years.length - 1}
+			<div class="bg-primary-500 ms-6 h-4 w-0.5"></div>
+		{/if}
 	{/each}
 </div>
--- app/src/lib/ui/TimeLineCard.svelte
+++ app/src/lib/ui/TimeLineCard.svelte
@@ -2,5 +2,19 @@
 	let { event } = $props();
 </script>
 
-<div>
-   </div>
+<div class="bg-surface-800 flex max-w-md flex-row rounded-3xl">
+	<aside class="flex min-w-20 items-center justify-center text-3xl bg-surface-600 rounded-l-3xl">
+		<span>
+			<span>{event.date.toLocaleDateString('en-UK', { day: 'numeric' })}</span>
+		</span>
+	</aside>
+	<div class="bg-surface-700 w-1 rounded-4xl"></div>
+	<article class=" p-4">
+		<header class="">
+			<strong>{event.title}</strong>
+		</header>
+		<article class="">
+			<p>{event.description}</p>
+		</article>
+	</article>
+</div>
