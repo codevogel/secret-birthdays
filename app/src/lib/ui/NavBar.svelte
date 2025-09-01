7d3f74bc88827179fb2c91ce0498ff99aa6805e2
Kamiel de Visser <kamieldevisser@gmail.com>
2025-09-01 16:52:56 +0200

    add logo navbar

--- app/src/lib/ui/NavBar.svelte
+++ app/src/lib/ui/NavBar.svelte
@@ -1,3 +1,9 @@
-<header class="bg-surface-700 p-4 text-center">
-	<h1 class="h2">Secret Birthdays</h1>
+<script lang="ts">
+	import { VenetianMask } from 'lucide-svelte';
+</script>
+
+<header class="bg-surface-700 flex flex-row justify-center p-4 text-center">
+	<h1 class="h2 font-logo text-primary-500">
+		<span class="flex items-center gap-x-4"><VenetianMask size={36} />Secret Birthdays</span>
+	</h1>
 </header>
