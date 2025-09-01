a2086489351183c9ee61bbdaed0f9ec9e59c45ac
Kamiel de Visser <kamieldevisser@gmail.com>
2025-09-01 16:53:42 +0200

    footer formatting

--- app/src/lib/ui/Footer.svelte
+++ app/src/lib/ui/Footer.svelte
@@ -1,6 +1,6 @@
-<footer class="p-4 bg-surface-900 text-center" >
+<footer class="bg-surface-900 p-4 text-center">
 	<p>
 		A site by
-		<a href="https://www.codevogel.com/" class="text-primary-500">CodeVogel</a>
+		<a href="https://www.codevogel.com/" class="text-primary-500 underline">CodeVogel</a>
 	</p>
 </footer>
